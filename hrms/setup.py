a8f7a32f4377b6c57eefd1548ef6b48e97e5e307
nareshkannasln <nareshkannashanmugam@gmail.com>
2026-01-22 17:55:31 +0530

    fix(user-type): add leave type read permission for employee self service
    (cherry picked from commit 4f4ada0ad4cf18b4663c18873a15ba2d3a1931c6)
    
    # Conflicts:
    #	hrms/patches.txt

--- hrms/setup.py
+++ hrms/setup.py
@@ -550,6 +550,7 @@ def get_post_install_patches():
 		"create_country_fixtures",
 		"update_allocate_on_in_leave_type",
 		"update_performance_module_changes",
+		"add_leave_type_permission_for_ess",
 	)
 
 
@@ -639,6 +640,7 @@ def get_user_types_data():
 				"Expense Claim Type": ["read"],
 				"Employee Advance": ["read", "write", "create", "delete"],
 				# leave and attendance
+				"Leave Type": ["read"],
 				"Leave Application": ["read", "write", "create", "delete"],
 				"Attendance Request": ["read", "write", "create", "delete"],
 				"Compensatory Leave Request": ["read", "write", "create", "delete"],
