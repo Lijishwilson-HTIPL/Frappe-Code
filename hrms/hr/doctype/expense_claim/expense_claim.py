19d00af50d5e94cb30fcd813e558d4787fc85b9b
iamkhanraheel <raheel@frappe.io>
2026-03-29 18:10:02 +0530

    refactor: add type hints in get_advance

--- hrms/hr/doctype/expense_claim/expense_claim.py
+++ hrms/hr/doctype/expense_claim/expense_claim.py
@@ -553,7 +553,7 @@ def get_expense_claim_account(expense_claim_type, company):
 
 
 @frappe.whitelist()
-def get_advances(employee, advance_id=None):
+def get_advances(employee: str, advance_id: str | None = None):
 	advance = frappe.qb.DocType("Employee Advance")
 
 	query = frappe.qb.from_(advance).select(
