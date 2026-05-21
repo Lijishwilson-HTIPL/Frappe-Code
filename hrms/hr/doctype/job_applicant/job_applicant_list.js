f0f568140ab2dbe855124937c5d754b240e67ee0
Hilton Paul <hilton.paul@hephzibahtech.com>
2026-05-21 14:59:01 +0530

    Interview/Job Applicant UI tweaks: applicant_name fetch, role label, list ID widening

--- hrms/hr/doctype/job_applicant/job_applicant_list.js
+++ hrms/hr/doctype/job_applicant/job_applicant_list.js
@@ -3,6 +3,11 @@
 
 frappe.listview_settings["Job Applicant"] = {
 	add_fields: ["status"],
+	formatters: {
+		name: function (value) {
+			return `<span style="white-space:nowrap;overflow:visible;display:inline-block;min-width:240px;" title="${frappe.utils.escape_html(value)}">${frappe.utils.escape_html(value)}</span>`;
+		},
+	},
 	get_indicator: function (doc) {
 		if (doc.status == "Accepted") {
 			return [__(doc.status), "green", "status,=," + doc.status];
